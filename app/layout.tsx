9fd8ccb9843e96e38c1f09cff6a743d9a9b605d7
EhsaasChaudhary <116794955+EhsaasChaudhary@users.noreply.github.com>
2024-09-23 19:49:06 +0530

    removed usestate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { Headbar } from "./components/headbar";
 import SidebarDemo from "./components/sidebar";
 import Footer from "./components/footer";
