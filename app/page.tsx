ecc0b21bd31e001fbc9ea51475b7c7ab40dd1fed
EhsaasChaudhary <116794955+EhsaasChaudhary@users.noreply.github.com>
2024-10-05 10:58:25 +0530

    updated home heading

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Component() {
             Welcome to Sanjivani
           </h1>
           <p className="text-lg md:text-xl lg:text-2xl text-white">
-            Discover <span className="font-bold">healthcare services</span> tailored just for you. Our platform offers <span className="font-bold">advanced management capabilities</span> you would not find anywhere else.
+            Discover <span className="font-bold">healthcare services</span> tailored just for you. Our platform offers <span className="font-bold">advanced management capabilities</span>.
           </p>
           <a href="/appointments" className="mt-6 inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600">
             Make an Appointment
